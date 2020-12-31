c14f9488768f07a3d7ab2e3b8bc43924883bd8f3
Hà Minh Cường <55825733+haminhcuong2k@users.noreply.github.com>
2021-01-01 00:47:26 +0700

    Add UI

--- app/src/main/java/com/example/anna/MainActivity.java
+++ app/src/main/java/com/example/anna/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.anna;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragment = new CoursesFragment();
+        loadFragment(fragment);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.courses:
+                    fragment = new CoursesFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.lookup:
+                    fragment = new LookUpFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.scan:
+                    fragment = new ScanFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.profile:
+                    fragment = new ProfileFragment();
+                    loadFragment(fragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/anna/ProfileFragment.java
@@ -0,0 +1,33 @@
+package com.example.anna;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ProfileFragment extends Fragment {
+
+    private NavController navController;
+
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
