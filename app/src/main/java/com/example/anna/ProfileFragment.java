20a4125bd15d962859fe50cd0fda06bfdd35d6ce
HUYNH HO THANH TRA <hothanhtrahuynh@github.com>
2021-01-03 15:25:33 +0700

    Add Signup and Login layout.  - layout/fragment_login.xml  - layout/fragment_profile.xml  - layout/fragment_signup.xml

--- app/src/main/java/com/example/anna/ProfileFragment.java
+++ app/src/main/java/com/example/anna/ProfileFragment.java
@@ -2,32 +2,69 @@ package com.example.anna;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfileFragment extends Fragment {
 
+    MainActivity main;
     private NavController navController;
-
+    LinearLayout signUpFragment,signInFragment;
+    Button signUpBtn,signInBtn;
 
     public ProfileFragment() {
         // Required empty public constructor
+    }
+    public static ProfileFragment newInstance(String strArg1){
+        ProfileFragment profileFragment = new ProfileFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("Arg1",strArg1);
+        profileFragment.setArguments(bundle);
+        return profileFragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        main = (MainActivity) getActivity();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        LinearLayout profileFm = (LinearLayout) inflater.inflate(R.layout.fragment_profile,null);
+        return profileFm;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        signUpBtn = (Button) view.findViewById(R.id.signUpProfileFm);
+        signInBtn = (Button) view.findViewById(R.id.signInProfileFm);
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_profileFragment_to_loginFragment);
+            }
+        });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_profileFragment_to_signUpFragment);
+            }
+        });
     }
 }
