82a23982e83e95759f3a2da174460a53200a999c
khuuthuyky123 <khuuthuyky123@gmail.com>
2021-01-13 03:28:16 +0700

    code ScanFragment

--- app/src/main/java/com/example/anna/ScanFragment.java
+++ app/src/main/java/com/example/anna/ScanFragment.java
@@ -1,12 +1,35 @@
 package com.example.anna;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.util.Pair;
+import androidx.fragment.app.Fragment;
+
+import com.example.anna.data.DBDictionaryManager;
+import com.example.anna.model.Word;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,7 +37,10 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ScanFragment extends Fragment {
-
+    ImageView imageView;
+    TextView scanText, translateText, title;
+    Button btnOpenCamera, btnTranlate, btnBack;
+    LinearLayout translate;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -58,7 +84,89 @@ public class ScanFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scan, container, false);
+        FrameLayout v = (FrameLayout) inflater.inflate(R.layout.fragment_scan, null);
+        imageView = v.findViewById(R.id.image);
+//        translateText = v.findViewById(R.id.tranlateText);
+        scanText = v.findViewById(R.id.scanText);
+        translate = v.findViewById(R.id.tranlate);
+        title = v.findViewById(R.id.title);
+        btnOpenCamera = v.findViewById(R.id.btnOpenCamera);
+        btnOpenCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 101);
+            }
+        });
+        btnTranlate = v.findViewById(R.id.btnTranlaste);
+        btnBack = v.findViewById(R.id.btnBack);
+        return v;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bundle bundle = data.getExtras();
+        Bitmap bitmap = (Bitmap) bundle.get("data");
+        imageView.setVisibility(View.VISIBLE);
+        imageView.setImageBitmap(bitmap);
+        final FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmap);
+        btnOpenCamera.setVisibility(View.GONE);
+        btnTranlate.setVisibility(View.VISIBLE);
+        btnTranlate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnTranlate.setVisibility(View.GONE);
+                btnBack.setVisibility(View.VISIBLE);
+                imageView.setVisibility(View.GONE);
+                translate.setVisibility(View.VISIBLE);
+                FirebaseVision firebaseVision = FirebaseVision.getInstance();
+                FirebaseVisionTextRecognizer firebaseVisionTextRecognizer = firebaseVision.getOnDeviceTextRecognizer();
+                Task<FirebaseVisionText> task = firebaseVisionTextRecognizer.processImage(firebaseVisionImage);
+                task.addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                    @Override
+                    public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                        String s = firebaseVisionText.getText();
+                        DBDictionaryManager dbDictionaryManager = new DBDictionaryManager(getActivity());
+                        //dbDictionaryManager.deleteDB();
+                        dbDictionaryManager.createDataBase();
+                        dbDictionaryManager.openDataBase();
+                        String[] scanWords = s.split("\n");
+                        ArrayList<Pair<String, String>> translateWords = new ArrayList<>();
+                        for (String word : scanWords) {
+                            Word w = dbDictionaryManager.searchWord(word);
+                            Pair<String, String> tmp;
+                            if (!w.getmVietnamese().equals("")) {
+                                tmp = new Pair<>(word, w.getmVietnamese());
+                                translateWords.add(tmp);
+                            }
+                        }
+                        String content = "";
+                        for (Pair<String, String> w : translateWords) {
+                            content += w.first + " : " + w.second + "\n";
+                        }
+                        title.setText("Tìm thấy " + translateWords.size() + " từ");
+                        scanText.setText(content);
+                    }
+                });
+                task.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+                btnBack.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        scanText.setText("");
+                        title.setText("");
+                        translate.setVisibility(View.GONE);
+//                        translateText.setText("");
+                        btnOpenCamera.setVisibility(View.VISIBLE);
+                        btnBack.setVisibility(View.GONE);
+                    }
+                });
+            }
+        });
     }
 }
